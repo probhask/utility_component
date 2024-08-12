4aff6866bb816266e2913ad5abd8159a4c87b3d7
Bhaskar Sharma <bhaskarneeraj23@gmail.com>
2024-08-12 17:12:48 +0530

    test - add test cases

---
+++ vitest.config.ts
@@ -0,0 +1,36 @@
+// import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./test/setup.ts",
+    coverage: {
+      exclude: [
+        "src/types/",
+        "src/routes",
+        "src/pages/Modal.tsx",
+        "**/*vite*",
+        "**/*tailwind*",
+        "**/*postcss*",
+        "**/*eslint*",
+      ],
+    },
+  },
+  coverage: {
+    exclude: ["**/types/**", "**/routes/**"],
+  },
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@routes": path.resolve(__dirname, "./src/routes"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+    },
+  },
+});
