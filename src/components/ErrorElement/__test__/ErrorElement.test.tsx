14742398459b855d4aa82ccebbef1bd89a283778
Bhaskar Sharma <bhaskarneeraj23@gmail.com>
2024-08-24 16:30:42 +0530

    remove - unused declartaion

--- src/components/ErrorElement/__test__/ErrorElement.test.tsx
+++ src/components/ErrorElement/__test__/ErrorElement.test.tsx
@@ -1,26 +1,14 @@
-import WidgetApp from "@container/WidgetApp";
-import { WidgetAppContext } from "@context/WidgetAppContext";
 import { render, screen } from "@testing-library/react";
-import { mockWidgetList } from "@utils/MockRenderWithContext";
+import App from "../../../App";
 import { MemoryRouter } from "react-router-dom";
-import { it, describe, vi } from "vitest";
+import { it, describe } from "vitest";
 
-const mockContextValue = {
-  widgetList: mockWidgetList,
-  handleAddNewWidget: vi.fn(),
-  deleteWidget: vi.fn(),
-  deleteCategory: vi.fn(),
-  toggleHideUnHide: vi.fn(),
-  handleSearch: vi.fn(),
-};
 describe("Error Element", () => {
   it("render Error Element", () => {
     render(
-      <WidgetAppContext.Provider value={mockContextValue}>
-        <MemoryRouter initialEntries={["/wrong-route"]}>
-          <WidgetApp />
-        </MemoryRouter>
-      </WidgetAppContext.Provider>
+      <MemoryRouter initialEntries={["/wrong-route"]}>
+        <App />
+      </MemoryRouter>
     );
     screen.debug();
   });
